210e1673bbf1035ec47c44c491022fc3b14a1c0e
jin <jinvoker@protonmail.com>
2020-04-16 19:55:01 +0530

    added dummy credits scene

---
+++ credits.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+
+
+func _process(delta):
+	position.y-=.5
+	
--- start_menu.gd
+++ start_menu.gd
@@ -21,7 +21,7 @@ func _on_start_pressed():
 
 
 func _on_credits_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene("res://credits.tscn")
 
 
 func _on_quit_pressed():
