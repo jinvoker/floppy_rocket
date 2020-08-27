aa19352cd6802e78b626f17e7c70e53e6070f916
Jin <Invokerjin@protonmail.com>
2020-08-28 00:11:49 +0530

    tweaked a few changes

--- src/credits.gd
+++ src/credits.gd
@@ -1,10 +1,17 @@
 extends Node2D
 
-
+export var upspeed=0.5
 
 func _process(delta):
-	position.y-=.5
+	translate(Vector2(0,-upspeed))
 	
 	if Input.is_action_just_pressed("flap"):
 		get_tree().change_scene("res://start_menu.tscn")
 		
+
+
+
+
+func _on_VisibilityNotifier2D_viewport_exited(viewport):
+	upspeed=0
+	pass # Replace with function body.
